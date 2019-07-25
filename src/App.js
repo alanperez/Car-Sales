3959d2add78669055fecdbba5afdb6ee3f5c3ae8
Alan Perez <alanp9six@gmail.com>
2019-07-24 22:36:55 -0500

    :ambulance: Changed the function name because having the same name wouldnt let me remove

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Total from './components/Total';
 const App = (props) => {
   
 
-  const removeFeature = item => {
+  const removeItem = item => {
     // dispatch an action here to remove an item
     props.removeFeature(item)
   };
@@ -25,7 +25,7 @@ const App = (props) => {
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures removeFeature={removeFeature} car={props.car}/>
+        <AddedFeatures removeItem={removeItem} car={props.car}/>
       </div>
       <div className="box">
         <AdditionalFeatures buyItem={buyItem} store={props.store} />
