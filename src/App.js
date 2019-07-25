c47d216b91307f0547f9b7ab85dc9655ad86f7a4
Alan Perez <alanp9six@gmail.com>
2019-07-24 20:52:51 -0500

    :zap: Redux store is setup.. changed state to props.. its displaying on screen now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {connect } from 'react-redux'
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = (props) => {
   
 
   const removeFeature = item => {
@@ -19,15 +20,22 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
-
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    store: state.store
+  }
+}
+export default connect()(App);
